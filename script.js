a123ab942ae58221cf8eb28f88d13b445c4aa2cc
Nicholas Oo <noo@umich.edu>
2018-07-24 20:52:08 -0400

    grid

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 console.log("yo");
 const container = document.querySelector('#container');
-for(var i = 0; i< 5; i++){
-	const content = document.createElement('div');
-	content.classList.add('square');
 
-	container.appendChild(content);
+for(var i = 0; i< 5; i++){
+	for(var j = 0; j < 5; j++){
+		const content = document.createElement('div');
+		content.classList.add('square');
+		if(j != 4){
+			content.classList.add('float');
+		}
+		container.appendChild(content);
+	}
 }
+
+const content = document.createElement('div');
+		content.classList.add('bigsquare');
+		container.appendChild(content);
