384b697453d9bfbb1686781d766f7bcb7f7efc71
Nicholas Oo <noo@umich.edu>
2018-08-02 10:06:19 -0400

    added initial color changes

--- script.js
+++ script.js
@@ -9,9 +9,32 @@ for(var i = 0; i< 5; i++){
 			content.classList.add('float');
 		}
 		container.appendChild(content);
-	}
+		content.addEventListener("mouseover", function(event){
+			//needs some more testing, at first i tried to use == "green" but I couldn't get it working yet
+			console.log("wassup");
+			if(content.style.background != "green" && content.style.background != "yellow"){
+				console.log("red!");
+				content.style.background = "green";
+			}
+			else if(content.style.background != "yellow"){
+				content.style.background = "yellow";
+			}
+			else console.log("lol nope");
+			console.log("done");
+			});
+			//content.style.background = "green";
+			//console.log("yo"); };
+		}
 }
 
-const content = document.createElement('div');
-		content.classList.add('bigsquare');
-		container.appendChild(content);
+
+
+function colorchange(content){
+	console.log("wassup");
+	if(content.style.background == "red"){
+		content.style.background == "green";
+	}
+	else if(content.style.background == "green"){
+		content.style.background == "yellow";
+	}
+}
